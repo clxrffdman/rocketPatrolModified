83698d417fb97ef53fa3315c1e03c1d6e55a4712
clxrffdman <clxrffdman@gmail.com>
2022-04-15 11:26:22 -0700

    -added header comment to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+// Calex Raffield, craffiel, Rocket Patrol Mods, 04/15/22, 2 hrs
+// POINT BREAKDOWN:
+// Simultanious Two-Player mode (30)
+// Display the time remaining (in seconds) on the screen (10)
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+// Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+// Create and implement a new weapon (w/ new behavior and graphics) (Missile with unique flight speed, acceleration and AOE Damage) (20)
+// Rocket acclerates throughout flight rather then staying at static speed (?)
+// Ships speed up with each kill (?) 
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
